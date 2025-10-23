68d57db23718863feb913e7fbeefda3cfce2e541
Andrey M. Mamedov <mam28.andr@yandex.ru>
2025-10-23 21:17:37 +0800

    В скрпт creater_proj.sh добавлено создание структуры каталогов будующего проекта

--- files/creater_proj.sh
+++ files/creater_proj.sh
@@ -2,8 +2,70 @@
 
 set -e
 
+# Проверка количества аргументов
+if [ $# -ne 1 ]; then
+    echo "Ошибка: Скрипт должен получать ровно один аргумент - имя каталога"
+    echo "Использование: $0 <имя_каталога>"
+    exit 1
+fi
+
+directory_name="$1"
+
+# Создание основного каталога
+echo "Создаю каталог: $directory_name"
+mkdir "$directory_name"
+
+# Проверка, что каталог создан успешно
+if [ ! -d "$directory_name" ]; then
+    echo "Ошибка: Не удалось создать каталог $directory_name"
+    exit 1
+fi
+
+echo "Каталог $directory_name успешно создан"
+
 # Создать структуру каталогов
-mkdir -p my_project/{docs,scripts,src/include,tests,third_party/doctest}
+
+echo "Создаю подкаталоги в $directory_name..."
+
+mkdir -p "$directory_name/docs"
+if [ -d "$directory_name/docs" ]; then
+    echo "Каталог $directory_name/docs успешно создан"
+else
+    echo "Невозможно создать каталог $directory_name/docs"
+fi
+
+mkdir -p "$directory_name/scripts"
+if [ -d "$directory_name/scripts" ]; then
+    echo "Каталог $directory_name/scripts успешно создан"
+else
+    echo "Невозможно создать каталог $directory_name/scripts"
+fi
+
+mkdir -p "$directory_name/src/include"
+if [ -d "$directory_name/src/include" ]; then
+    echo "Каталог $directory_name/src/include успешно создан"
+else
+    echo "Невозможно создать каталог $directory_name/src/include"
+fi
+
+mkdir -p "$directory_name/tests"
+if [ -d "$directory_name/tests" ]; then
+    echo "Каталог $directory_name/tests успешно создан"
+else
+    echo "Невозможно создать каталог $directory_name/tests"
+fi
+
+mkdir -p "$directory_name/third_party/doctest"
+if [ -d "$directory_name/third_party/doctest" ]; then
+    echo "Каталог $directory_name/third_party/doctest успешно создан"
+else
+    echo "Невозможно создать каталог $directory_name/third_party/doctest"
+fi
 
 # Скачать файлы
-wget -P ./third_party/doctest/ https://github.com/doctest/doctest/blob/master/doctest/doctest.h
+#wget -P ./third_party/doctest/ https://github.com/doctest/doctest/blob/master/doctest/doctest.h
+
+echo "Структура созданного каталога:"
+tree "$directory_name" 2>/dev/null || ls -la "$directory_name"
+
+echo "Операция завершена успешно!"
